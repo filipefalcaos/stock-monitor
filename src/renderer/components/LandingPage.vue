87bd0633f2a5fb855150ac478b585507e8e8be31
Filipe Falcão <filipefalcaosantos@gmail.com>
2020-04-09 04:11:57 -0300

    [feat] Gets the prices of the stocks from UOL and updates the view.

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -15,7 +15,7 @@
             <b-table-column field="amount" label="Quantidade" :numeric="true">
               {{ props.row.amount }}
             </b-table-column>
-            <b-table-column field="current_price" label="Preço Atual" :numeric="true">
+            <b-table-column :class="{ 'green-success': new_data }" field="current_price" label="Preço Atual" :numeric="true">
               R$ {{ props.row.current_price }}
             </b-table-column>
             <b-table-column field="result" label="Resultado" :numeric="true">
@@ -34,7 +34,7 @@
         <h1 class="title is-5">Resultado Percentual = {{ percent_result }}% </h1>
 
         <div class="buttons">
-          <b-button type="is-info" expanded>Atualizar</b-button>
+          <b-button @click="get_stock_prices" type="is-info" expanded>Atualizar</b-button>
           <b-button type="is-success" expanded>Salvar Excel</b-button>
         </div>
       </div>
@@ -100,27 +100,42 @@
     data () {
       return {
         full_value: 100000,
+        new_data: false,
         stock_data_buy: [
-          { 'stock': 'AZUL4', 'first_price': 16.96, 'amount': 600, 'current_price': 15.99 },
-          { 'stock': 'RAPT4', 'first_price': 6.08, 'amount': 1700, 'current_price': 6.35 },
-          { 'stock': 'IRBR3', 'first_price': 10.66, 'amount': 1000, 'current_price': 10.68 },
-          { 'stock': 'CEAB3', 'first_price': 7.82, 'amount': 1300, 'current_price': 8.26 },
-          { 'stock': 'UGPA3', 'first_price': 13.58, 'amount': 700, 'current_price': 13.18 },
-          { 'stock': 'VIVA3', 'first_price': 15.55, 'amount': 700, 'current_price': 15.49 },
-          { 'stock': 'ALSO3', 'first_price': 26.71, 'amount': 400, 'current_price': 26.42 },
-          { 'stock': 'LINX3', 'first_price': 20.29, 'amount': 500, 'current_price': 20.29 },
-          { 'stock': 'MRVE3', 'first_price': 13.12, 'amount': 700, 'current_price': 12.69 },
-          { 'stock': 'YDUQ3', 'first_price': 26.10, 'amount': 400, 'current_price': 26.91 }
+          { 'stock': 'AZUL4', 'uol_code': 1881, 'first_price': 16.96, 'amount': 600, 'current_price': 15.99 },
+          { 'stock': 'RAPT4', 'uol_code': 522, 'first_price': 6.08, 'amount': 1700, 'current_price': 6.35 },
+          { 'stock': 'IRBR3', 'uol_code': 1914, 'first_price': 10.66, 'amount': 1000, 'current_price': 10.68 },
+          { 'stock': 'CEAB3', 'uol_code': 2395, 'first_price': 7.82, 'amount': 1300, 'current_price': 8.26 },
+          { 'stock': 'UGPA3', 'uol_code': 674, 'first_price': 13.58, 'amount': 700, 'current_price': 13.18 },
+          { 'stock': 'VIVA3', 'uol_code': 2388, 'first_price': 15.55, 'amount': 700, 'current_price': 15.49 },
+          { 'stock': 'ALSO3', 'uol_code': 2329, 'first_price': 26.71, 'amount': 400, 'current_price': 26.42 },
+          { 'stock': 'LINX3', 'uol_code': 1404, 'first_price': 20.29, 'amount': 500, 'current_price': 20.29 },
+          { 'stock': 'MRVE3', 'uol_code': 451, 'first_price': 13.12, 'amount': 700, 'current_price': 12.69 },
+          { 'stock': 'YDUQ3', 'uol_code': 2324, 'first_price': 26.10, 'amount': 400, 'current_price': 26.91 }
         ],
         stock_data_sell: [
-          { 'stock': 'BOVA11', 'first_price': 75.75, 'amount': 1340, 'current_price': 73.51 }
+          { 'stock': 'BOVA11', 'uol_code': 1027, 'first_price': 75.75, 'amount': 1340, 'current_price': 73.51 }
         ]
       }
     },
     
     methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
+      get_stock_prices() {
+        this.stock_data_buy.forEach(stock => {
+          this.$http
+            .get('http://cotacoes.economia.uol.com.br/ws/asset/' + stock.uol_code + '/intraday?size=1')
+            .then(response => (stock.current_price = response.data.data[0].price))
+        });
+
+        this.stock_data_sell.forEach(stock => {
+          this.$http
+            .get('http://cotacoes.economia.uol.com.br/ws/asset/' + stock.uol_code + '/intraday?size=1')
+            .then(response => {
+              stock.current_price = response.data.data[0].price;
+              this.new_data = true;
+              setTimeout(() => { this.new_data = false; }, 2000);
+            });
+        });
       }
     }
   }
@@ -139,6 +154,10 @@
   body { font-family: 'Source Sans Pro', sans-serif; }
 
   .section {
-    padding: 1.5rem 1.5rem!important;
+    padding: 2rem 2rem!important;
+  }
+
+  .green-success {
+    color: hsl(141, 53%, 53%)
   }
 </style>
