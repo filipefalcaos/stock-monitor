94b258562860d8e22f27b3764b73b382ae93b7a0
Filipe Falcão <filipefalcaosantos@gmail.com>
2021-02-05 05:07:15 -0300

    [refact] Removes the Buefy loading overlay in favor of a built-in one, with only a minor dependency.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
-    <b-loading
-      v-model="isLoading"
-      :class="{ 'initial-loading': appCreated }"
-    />
+    <spinner-overlay :active="isLoading" />
     <router-view />
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import SpinnerOverlay from './components/containers/SpinnerOverlay'
 
 export default {
   name: 'App',
+  components: { SpinnerOverlay },
 
   computed: {
     ...mapState({
@@ -34,8 +33,4 @@ export default {
   @import "~buefy/dist/buefy.css";
   @import 'assets/scss/style';
   @import url('https://cdn.materialdesignicons.com/5.3.45/css/materialdesignicons.min.css');
-
-  .initial-loading {
-    background-color: rgba(255, 255, 255, 1)
-  }
 </style>
---
+++ src/components/containers/SpinnerOverlay.vue
@@ -0,0 +1,71 @@
+<template>
+  <transition name="fade">
+    <div
+      v-if="active"
+      class="overlay"
+    >
+      <div class="overlay-background" />
+      <spinner
+        size="big"
+        line-fg-color="#ced2d8"
+        :speed="1"
+      />
+    </div>
+  </transition>
+</template>
+
+<script>
+import Spinner from 'vue-simple-spinner'
+
+export default {
+  name: 'SpinnerOverlay',
+  components: { Spinner },
+  props: {
+    active: {
+      type: Boolean,
+      default: true
+    }
+  },
+
+  watch: {
+    active: function(val) {
+      if (val) {
+        document.body.classList.add('overlay-shown')
+        document.documentElement.classList.add('overlay-shown')
+      } else {
+        document.body.classList.remove('overlay-shown')
+        document.documentElement.classList.remove('overlay-shown')
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .overlay {
+    bottom: 0;
+    left: 0;
+    position: fixed;
+    right: 0;
+    top: 0;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+    z-index: 5;
+  }
+
+  .overlay-background {
+    bottom: 0;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    background: #fff;
+    opacity: 0.5;
+  }
+
+  .overlay-shown {
+    overflow: hidden;
+  }
+</style>
